5a356b53f1c8f83fe3be62a18702963892c78941
Aritra Chakraborty <ar_itra@yahoo.com>
2018-05-18 05:45:37 +0530

    added java files

---
+++ PackageManagerInterface.java
@@ -0,0 +1,7 @@
+public class PackageManagerInterface{
+    
+    public static void main(String[] args) throws Exception{
+        PackageManager p = new PackageManager();
+        p.start();
+    }
+}
