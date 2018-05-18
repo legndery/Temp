454d7cc68b070fbea07cebd1c182593b6b0ceb3d
Aritra Chakraborty <ar_itra@yahoo.com>
2018-05-18 12:59:23 +0530

    d

--- PackageManagerInterface.java
+++
@@ -1,7 +0,0 @@
-public class PackageManagerInterface{
-    
-    public static void main(String[] args) throws Exception{
-        PackageManager p = new PackageManager();
-        p.start();
-    }
-}
